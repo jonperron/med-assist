bccba8fe3dad5ea035f9e0cfdded0d55d9101e18
Jonathan Perron <jonathan.perron@lumapps.com>
2025-07-13 14:32:08 +0200

    feat(api): update extraction route with placeholder and 404 response

--- backend/app/api/routes/extractions.py
+++ backend/app/api/routes/extractions.py
@@ -1,24 +1,35 @@
-from fastapi import APIRouter
+from app.core.dependencies import get_redis_storage
+from app.db.redis import RedisStorage
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 
 @router.get("/get_extracted_text/{file_id}")
-async def get_extracted_text(file_id: str):
+async def get_extracted_text(
+    file_id: str,
+    redis_storage: RedisStorage = Depends(get_redis_storage),
+):
     """
-    Endpoint to retrieve extracted text (placeholder).
+    Endpoint to retrieve extracted text.
     """
-    # if file_id in extracted_data_store:
-    #     # This will later return actual extracted entities
-    #     return JSONResponse(
-    #         status_code=200,
-    #         content={
-    #             "file_id": file_id,
-    #             "data": extracted_data_store[file_id],
-    #             "extracted_entities": "Placeholder for diseases, symptoms, treatments"
-    #         }
-    #     )
+    extracted_text = await redis_storage.get_value(file_id)
+
+    if not extracted_text:
+        return JSONResponse(
+            status_code=404, content={"message": "File not found or not processed yet."}
+        )
+
     return JSONResponse(
-        status_code=404, content={"message": "File not found or not processed yet."}
+        status_code=200,
+        content={
+            "file_id": file_id,
+            "text": extracted_text,
+            "extracted_entities": {
+                "diseases": [],
+                "symptoms": [],
+                "treatments": [],
+            },  # Placeholder
+        },
     )
